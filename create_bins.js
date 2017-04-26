76ccfac2f44531878b6ee1bad00a6146a7c3ecb2
Qiming HOU <hqm03ster@gmail.com>
2017-04-26 16:53:08 +0800

    published

---
+++ create_bins.js
@@ -0,0 +1,35 @@
+'use strict'
+const child_process=require('child_process');
+const path=require('path');
+const fs=require('fs');
+
+/*
+We need to manually create wrappers for Windows:
+	Window BAT (sshex.cmd):
+	Cygwin shell (sshex.sh):
+	Unix (sshex.js):
+		just create a symlink
+*/
+(function(){
+	if(process.argv.length<3){
+		return;
+	}
+	var path_bin_root=child_process.execSync('npm bin -g').toString().replace(/\n/g,'');
+	//npm bin -g
+	if(process.argv[2]==='install'){
+		if(process.platform==='win32'){
+			fs.writeFileSync(path.join(path_bin_root,'sshex.cmd'),fs.readFileSync(path.join(__dirname,'sshex.cmd')));
+			fs.writeFileSync(path.join(path_bin_root,'sshex'),fs.readFileSync(path.join(__dirname,'sshex.sh')));
+		}else{
+			fs.symlinkSync(path.join(path_bin_root,'sshex'),path.join(__dirname,'sshex.js'));
+			fs.chmodSync(path.join(__dirname,'sshex.js'),0o755);
+		}
+	}else{
+		if(process.platform==='win32'){
+			fs.unlinkSync(path.join(path_bin_root,'sshex.cmd'));
+			fs.unlinkSync(path.join(path_bin_root,'sshex'));
+		}else{
+			fs.unlinkSync(path.join(path_bin_root,'sshex'));
+		}
+	}
+})();
---
+++ readme.md
@@ -0,0 +1,7 @@
+An enhanced SSH client where you could press CTRL+Q to access a SFTP prompt at any time.
+
+Install:
+```
+npm install -g sshex
+sshex --help
+```
